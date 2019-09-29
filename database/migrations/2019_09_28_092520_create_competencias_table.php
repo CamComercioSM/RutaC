161514d0a8fd3b311b86eddeac08562350d52acb
Miguel Cotes <miguel5230@gmail.com>
2019-09-28 23:36:27 -0500

    Se crean migraciones

--- database/migrations/2019_09_28_092520_create_competencias_table.php
+++ database/migrations/2019_09_28_092520_create_competencias_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCompetenciasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('competencias', function (Blueprint $table) {
+            $table->increments('competenciaID');
+            $table->string('competenciaNOMBRE',255);
+            $table->enum('competenciaESTADO',['Activo', 'Inactivo'])->default('Activo');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('competencias');
     }
 }
