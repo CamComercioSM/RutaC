f6cd9b1b6bbcd074ab8723230aa6c8baa889c038
Miguel Cotes <miguel5230@gmail.com>
2019-10-01 23:12:59 -0500

    Se hacen correcciones y mejoras

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -103,6 +103,12 @@
 
 <script src="{{ asset('dist/js/demo.js') }}"></script>
 
+<script src="{{ asset('bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+
+<script src="{{ asset('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+
+<script src="{{ asset('js/scripts.js') }}"></script>
+
 @yield('footer')
 
 </body>
