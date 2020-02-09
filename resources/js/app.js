f57288ee7c523a1a8a26fd0cd47a45f342029f48
mcotes <miguel5230@gmail.com>
2020-02-08 23:24:54 -0500

    Se agregan librerias VUE

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 require('./bootstrap');
+require('./font-awesome');
+require('./vee-validate');
+
+window.Vue = require('vue');
+
+// Bootstrap-vue
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+
+// Custom components
+import RCSelect from "./components/inputs/RCSelect";
+import RCInput from "./components/inputs/RCInput";
+
+Vue.component('rc-select', RCSelect);
+Vue.component('rc-input', RCInput);
+
+const app = new Vue({
+    el: '#app',
+});
