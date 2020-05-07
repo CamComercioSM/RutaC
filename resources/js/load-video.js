1ed8686ae1f0d61e09aeed4b78602b4c1b0121a1
mcotes <miguel5230@gmail.com>
2020-05-06 22:39:45 -0500

    Cambio en url

--- resources/js/load-video.js
+++ resources/js/load-video.js
@@ -4,7 +4,7 @@ $('#videoEstacion').on('show.bs.modal', function (e) {
     let estacion = $(e.relatedTarget).data('estacion');
     let ruta = $(e.relatedTarget).data('ruta');
 
-    axios.get('http://localhost/innovando/RutaC/public/user/marcar-estacion/'+estacion+'/'+ruta, {})
+    axios.get('https://rutadecrecimiento.com/user/marcar-estacion/'+estacion+'/'+ruta, {})
         .then(function (response) {
             console.log(response);
             document.getElementById("icCumplimiento-"+estacion).innerHTML = '<i class="fas fa-check-circle text-success mr-2"></i>';
